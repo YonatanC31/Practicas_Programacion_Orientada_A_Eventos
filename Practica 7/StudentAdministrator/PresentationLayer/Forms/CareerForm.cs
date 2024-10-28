9ba3f11c735a886e45fb17ac6e582a246c741f98
yonatanc31 <chicasyonatan70@gmail.com>
2024-10-28 10:31:00 -0600

    Practicas

---
+++ Practica 7/StudentAdministrator/PresentationLayer/Forms/CareerForm.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Services;
+using CommonLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer.Forms
+{
+	public partial class CareerForm : Form
+	{
+		private CareerService _careerService;
+		bool isEditing = false;
+
+		public CareerForm()
+		{
+			_careerService = new CareerService();
+			InitializeComponent();
+			LoadCareerData();
+		}
+
+		private void LoadCareerData()
+		{
+			dgvCareers.DataSource = _careerService.GetAllDoctors();
+		}
+
+	}
+}
